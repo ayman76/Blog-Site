3644bd9065590e25e176c8a32605ea32883e0a45
Ayman <a.m_ayman9@outlook.com>
2023-09-09 15:05:06 +0300

    Adding fillable attribute in Post Model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+
+    // We have two ways to make our attributes be fillable and accept values to insert
+
+    //1- Using Fillable which we give it all attributes that we need to give it values
+    protected $fillable = ['title', 'excerpt', 'body'];
+
+    //2- Using guarded which we give it attributes that we don't need to give it values to be inserted
+    //like 'id' which it auto increment in database
+
+    // protected $guarded = ['id'];
+
 }
