b4b42d2e99d32f6561d5183548a987e520399261
Ayman <a.m_ayman9@outlook.com>
2023-09-10 15:35:51 +0300

    Adding new template to Blog site

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,6 +1,27 @@
 @extends('components.layout')
 
-@section('header')
+@section('content')
+    @include('posts_header')
+
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+
+        @include('components.post_featured')
+
+        <div class="lg:grid lg:grid-cols-2">
+            @include('components.post_card')
+            @include('components.post_card')
+        </div>
+
+        <div class="lg:grid lg:grid-cols-3">
+            @include('components.post_card')
+            @include('components.post_card')
+            @include('components.post_card')
+
+
+        </div>
+    </main>
+
+    {{-- @section('header')
     <h1> Blogs </h1>
 @endsection
 
@@ -22,5 +43,5 @@
                 {!! $post->excerpt !!}
             </div>
         </article>
-    @endforeach
+    @endforeach --}}
 @endsection
